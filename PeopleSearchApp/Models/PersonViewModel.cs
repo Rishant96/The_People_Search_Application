cec69d5aaae2b88f251d9b09e47be7e0ab72c8c4
Rishant96 <rishant.dutt@gmail.com>
2020-04-01 11:40:54 -0400

    ViewModel added, minor changes to manifest.json

---
+++ PeopleSearchApp/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleSearchApp.Models
+{
+    public class PersonViewModel
+    {
+    }
+}
